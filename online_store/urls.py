eb5e0becc3b957975414ee940b1b1721be58fbb0
Swetlana Mirzoeva <levka008@mail.ru>
2025-11-04 19:08:08 +0300

    added README.md

--- online_store/urls.py
+++ online_store/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include  # 📍 ДОБАВЬ include!
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("catalog.urls")),  # 📍 ПОДКЛЮЧАЕМ ПРИЛОЖЕНИЕ CATALOG
+    path("", include("catalog.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
