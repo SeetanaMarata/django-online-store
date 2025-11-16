312014e732ca14315645517e289d278b0c85af6a
Swetlana Mirzoeva <levka008@mail.ru>
2025-11-16 18:30:31 +0300

    feat: configure URL routing for blog application
    - Create blog/urls.py with namespace and URL patterns
    - Implement routes for list, detail, create, edit, delete operations
    - Connect blog URLs to main project URL configuration
    - Use app_name='blog' for proper namespace isolation
    - Setup RESTful URL structure for blog operations

--- online_store/urls.py
+++ online_store/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include  # 📍 ДОБАВЬ include!
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls")),
+    path("blog/", include("blog.urls")),
 ]
 
 if settings.DEBUG:
